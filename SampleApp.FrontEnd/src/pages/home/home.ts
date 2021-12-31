95dc3d5c7293194a567243cb963e378b36a72306
suleyman.danalioglu <suleyman.danalioglu@arabam.com>
2021-12-31 23:05:23 +0300

    add: sass files

--- SampleApp.FrontEnd/src/pages/home/home.ts
+++ SampleApp.FrontEnd/src/pages/home/home.ts
@@ -1,15 +1,16 @@
+import "./home.scss";
 import { createApp, ref } from "vue";
 
 const homePageApp = createApp({
   setup() {
     const title = ref("Home Page");
     const message = ref("This message coming from Vite + Vue");
-    const count = ref(0)
+    const count = ref(0);
 
     return {
       title,
       message,
-      count
+      count,
     };
   },
 });
