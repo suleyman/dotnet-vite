56903066f5e7e5da8005e275bdf31775d404d24a
suleyman <slymnd28@gmail.com>
2022-01-03 15:46:06 +0300

    add: contact page & form

---
+++ SampleApp.FrontEnd/src/pages/contact/components/contact-form.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Form v-slot="{ values }">
+      <Field name="email" type="email" />
+      <Field name="password" type="password" />
+      <!-- print form values -->
+      <pre>{{ values }}</pre>
+    </Form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Form, Field } from "vee-validate";
+
+export default defineComponent({
+  name: "contact-form",
+  components: {
+    Form,
+    Field
+  },
+  setup() {
+
+  }
+});
+</script>
--- SampleApp.FrontEnd/src/pages/contact/contact.ts
+++ SampleApp.FrontEnd/src/pages/contact/contact.ts
@@ -1,7 +1,11 @@
-import "./contact.scss"
+import "./contact.scss";
 import { createApp, ref } from "vue";
+import ContactForm from "./components/contact-form.vue";
 
 const contactPageApp = createApp({
+  components: {
+    ContactForm,
+  },
   setup() {
     const title = ref("Contact Page");
     const message = ref("This message coming from Vite + Vue");
